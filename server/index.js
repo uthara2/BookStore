0d3f90f2a6649693a3ccf19829b6a9d9e6ef0688
Uthara <uthara852@gmail.com>
2026-05-25 15:59:58 +0530

    Setup backend server and MongoDB connection

---
+++ server/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`); 
+});
